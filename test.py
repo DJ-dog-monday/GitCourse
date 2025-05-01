a2dca08e7e2c29d2bb96ae3be4196236bb889e3a
DJ-dog-monday <dan.ogardo@gmail.com>
2025-05-02 01:23:37 +0800

    seccond commit

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-from sys import exit
+# from sys import exit
 
-def exit():
-    print('I wanna exit')
+# def exit():
+#     print('I wanna exit')
     
-exit()
+# exit()
+
+
+import math
+
+# for name in dir(math):
+#     print(name, end='\t')
+    
+#need to know 6 function in math module for PCAP
+
+print(math.ceil(3.6))
+print(math.floor(3.6))
+print(math.trunc(3.6))
+
+print(math.ceil(-3.6))
+print(math.floor(-3.6))
+print(math.trunc(-3.6))
